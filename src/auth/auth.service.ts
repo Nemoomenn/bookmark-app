2be31c53e4c09522b7837111dc905b6b96b8b97a
Nemoomenn <nemoomenbackoffice@gmail.com>
2022-05-27 20:55:56 +0545

    setup auth module with dependency injection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+   
+    signup() {
+        return {message: 'Signup End Point'}
+    }
+
+    login() {
+        return {message: 'Login End Point'}
+    }
+
+ }
