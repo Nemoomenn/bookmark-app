d0387df1c69aad4ffc5d5245b9afd6e751fdef0b
Nemoomenn <nemoomenbackoffice@gmail.com>
2022-05-27 20:47:28 +0545

    fix some git issue, create controller and service file for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule],
+  imports: [AuthModule],
 })
 export class AppModule {}
